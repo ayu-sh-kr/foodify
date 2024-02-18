8e0fae1b0e434afae247d62151b8b97341d99de3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-02-18 23:32:55 +0530

    Code refactoring and adjustment

--- src/app/restaurant-main/restaurant-order-online/restaurant-order-online.component.ts
+++ src/app/restaurant-main/restaurant-order-online/restaurant-order-online.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {OrderCombo} from "../../utils/types/BasicTypes";
+import {OrderFilterComponent} from "./order-filter/order-filter.component";
+import {OrderContentComponent} from "./order-content/order-content.component";
 
 @Component({
-  selector: 'app-restaurant-order-online',
-  standalone: true,
-  imports: [],
-  templateUrl: './restaurant-order-online.component.html',
-  styleUrl: './restaurant-order-online.component.css'
+    selector: 'app-restaurant-order-online',
+    standalone: true,
+    imports: [
+        OrderFilterComponent,
+        OrderContentComponent
+    ],
+    templateUrl: './restaurant-order-online.component.html',
+    styleUrl: './restaurant-order-online.component.css'
 })
 export class RestaurantOrderOnlineComponent {
 
